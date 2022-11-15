2c55c6e7699f159d0fd101935240b2163c7f4846
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 20:30:52 +0530

    Update streamlit_app.py
    Added pandas code to display a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st;
+import pandas as pd;
 
 st.title("Mel and Igor's new healthy diner!");
 
@@ -8,3 +9,7 @@ st.text("\N{stuffed flatbread} Poha mixed not fried!");
 st.text("\N{bread} \N{leafy green} \N{egg} Roti and Subzi and Boiled egg");
 st.text("\N{hot beverage} Healthy wali Chai");
 st.header("\N{grapes} \N{watermelon} \N{banana} DYI Smoothie \N{mango} \N{pineapple} \N{red apple}");
+
+#read a csv file and display it using pandas and streamlit
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+st.dataframe(my_fruit_list);
