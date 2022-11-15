f077238dfd3344075b41b3cb51cef25abec829e8
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:27:37 +0530

    Update streamlit_app.py
    fixed typo!

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st;
 st.title("Mel and Igor's new healthy diner!");
 
 st.header("Breakfast menu - not for the faint hearted");
-st.text("Idli + Sambhar chuntney");
+st.text("Idli + Sambhar chutney");
 st.text("Poha mixed, not fried!");
 st.text("Roti + Subzi + Boiled egg");
