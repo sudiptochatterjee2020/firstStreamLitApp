b5ccffc614ee366a3270869e1509b60e1621655d
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:25:31 +0530

    Update streamlit_app.py
    Added contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st;
 
 st.title("Mel and Igor's new healthy diner!");
+
+st.header("Breakfast menu");
+st.text("Idli + Sambhar chuntney");
+st.text("Poha mixed, not fried!");
+st.text("Roti + Subzi + Boiled egg");
