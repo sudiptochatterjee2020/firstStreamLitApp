73b4787817657cb762463a74f8b0f2041f62ecc8
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:55:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st;
 st.title("Mel and Igor's new healthy diner!");
 
 st.header("\N{flexed biceps} Breakfast Menu - Champs Only \N{flexed biceps}");
-
+st.text("\N{pot of food} Idli + Sambhar Chutney");
 st.text("\N{stuffed flatbread} Poha mixed not fried!");
-st.text("\N{naan} \N{leafy green} \N{egg} Roti and Subzi and Boiled egg");
+st.text("\N{bread} \N{leafy green} \N{egg} Roti and Subzi and Boiled egg");
 st.text("\N{hot beverage} Healthy wali Chai");
