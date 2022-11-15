3f0e3b7c5f4d6e450ffaf2edd50ed307bc3d4139
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:30:18 +0530

    Update streamlit_app.py
    Added to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.header("Breakfast menu - not for the faint hearted");
 st.text("Idli + Sambhar chutney");
 st.text("Poha mixed, not fried!");
 st.text("Roti + Subzi + Boiled egg");
+st.text("function(Chai,adrak, honey,-dudh,-shakkar) = healthy Chai!"); 
