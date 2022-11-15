aa7a8b27b24cb94ca65421699f9d4015a58ecdcf
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:27:07 +0530

    Update streamlit_app.py
    Modified the breakfast menu caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st;
 
 st.title("Mel and Igor's new healthy diner!");
 
-st.header("Breakfast menu");
+st.header("Breakfast menu - not for the faint hearted");
 st.text("Idli + Sambhar chuntney");
 st.text("Poha mixed, not fried!");
 st.text("Roti + Subzi + Boiled egg");
