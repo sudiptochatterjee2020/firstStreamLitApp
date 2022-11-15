81715dc292cc413595d6f55cd553b477340d2263
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-15 19:45:37 +0530

    Update streamlit_app.py
    Added all emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st;
 
 st.title("Mel and Igor's new healthy diner!");
 
-st.header('\N{flexed biceps} Breakfast Menu - Champs Only \N{flexed biceps}')
-st.text("Idli + Sambhar chutney");
-st.text("Poha mixed, not fried!");
-st.text("Roti + Subzi + Boiled egg");
-st.text("function(Chai,adrak, honey,-dudh,-shakkar) = healthy Chai!");
+st.header("\N{flexed biceps} Breakfast Menu - Champs Only \N{flexed biceps}")
+st.text("\N{curry rice} Idli + Sambhar chutney");
+st.text("\N{stuffed flatbread} Poha mixed, not fried!");
+st.text("\N{flatbread} \N{leafy green} \N{egg} Roti + Subzi + Boiled egg");
+st.text("\N{hot beverage} function(Chai,adrak, honey,-dudh,-shakkar) = healthy Chai!");
