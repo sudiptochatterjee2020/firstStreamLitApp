b581edeec462b90653e6452cf6524a627da0063d
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-16 09:43:14 +0530

    Update streamlit_app.py
    Added a multi select widget
    User can now select multiple items from a list

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,8 @@ st.header("\N{grapes} \N{watermelon} \N{banana} DYI Smoothie \N{mango} \N{pineap
 
 #read a csv file and display it using pandas and streamlit
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+# Set the fruit name as the index
+my_fruit_list = my_fruit_list.set_index('Fruit');
+# Add a multiselect widget
+st.multiselect("Pick some fruits:", list(my_fruit_list.index));
 st.dataframe(my_fruit_list);
