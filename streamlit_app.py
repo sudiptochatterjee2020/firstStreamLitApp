544fbff3f1ddda1f7b746661265a9cbc60034c12
sudiptochatterjee2020 <68953817+sudiptochatterjee2020@users.noreply.github.com>
2022-11-17 17:21:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ st.text("\N{pot of food} Idli + Sambhar Chutney");
 st.text("\N{stuffed flatbread} Poha mixed not fried!");
 st.text("\N{bread} \N{leafy green} \N{egg} Roti and Subzi and Boiled egg");
 st.text("\N{hot beverage} Healthy wali Chai");
-st.header("\N{grapes} \N{watermelon} \N{banana} DYI Smoothie \N{mango} \N{pineapple} \N{red apple}");
 
+st.header("\N{grapes} \N{watermelon} \N{banana} DYI Smoothie \N{mango} \N{pineapple} \N{red apple}");
 #read a csv file and display it using pandas and streamlit
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 # Set the fruit name as the index
@@ -21,5 +21,6 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 # filter the pandas dataframe and only display to the user the selected fruits 
 st.dataframe(my_fruit_list.loc[fruits_selected]); 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.header("Fruityvice Fruit Advice!");
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
+st.text(fruityvice_response.json());
