9d3f421a9a434256e818ac1effb5dc8f4f02801e
sudiptochatterjee2020 <sudiptochatterjee2020@gmail.com>
2022-11-22 20:24:57 +0530

    querying snowflake from python using a snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,6 @@ my_cur.execute("select * from fruit_load_list");
 my_data_row = my_cur.fetchall();
 st.header("The fruit load list contains");
 st.dataframe(my_data_row);
+
+fruit_choice2 = st.text_input('What fruit would you like to add?');
+st.write('Thanks for adding ', fruit_choice2);
