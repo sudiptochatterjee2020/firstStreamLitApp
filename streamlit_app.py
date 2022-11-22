74e856a1f79b6e7115f44db0dcd95a75adb217c4
sudiptochatterjee2020 <sudiptochatterjee2020@gmail.com>
2022-11-22 13:30:11 +0530

    corrected errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json());
 # and display the pandas dataframe using streamlit  
 st.dataframe(fruityvice_normalized);
 
-my_cnx = cnx.connect(**streamlit.secrets["snowflake"])
+my_cnx = cnx.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
