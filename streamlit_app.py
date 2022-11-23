68d098ff866aca0dc421c1ac4910e26f6ec2ff8e
sudiptochatterjee2020 <sudiptochatterjee2020@gmail.com>
2022-11-23 08:38:25 +0530

    added an insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,5 @@ st.dataframe(my_data_row);
 
 fruit_choice2 = st.text_input('What fruit would you like to add?');
 st.write('Thanks for adding ', fruit_choice2);
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit)");
